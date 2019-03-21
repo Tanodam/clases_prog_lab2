72edb8fde105929318da78038067c749ff499f32
tanodam <desariod95@gmail.com>
2019-03-20 21:52:47 -0300

    Clase 1 20/03/2019

---
+++ clases_prog_lab2/Clase 1/Ejercicio4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int s;
+            int a;
+            int numero;
+            int j;
+
+            Console.Write("Determine hasta que numero desea realizar la busqueda de numeros perfectos: ");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numero; i++)
+            {
+                b = 0;
+                s = i / 2;
+
+                for (j = 1; j <= s; j++)
+                {
+
+                    a = i % j;
+
+                    if (a == 0)
+                        b = b + j;
+                }
+                if (b == i)
+                    Console.WriteLine("El numero " + i + " es perfecto y su divisores son: " + s);
+
+            }
+            Console.ReadKey();
+        }
+}
+}
+
+  
