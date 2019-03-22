d954a8b14c0e7b149b762b65a85db169481763d5
tanodam <desariod95@gmail.com>
2019-03-21 21:55:01 -0300

    Clase 2 21/03/2019

--- clases_prog_lab2/Clase 1/Ejercicio7/Program.cs
+++ clases_prog_lab2/Clase 1/Ejercicio7/Program.cs
@@ -14,18 +14,30 @@ namespace Ejercicio7
             DateTime fechaNacimiento;
             DateTime hoy;
             TimeSpan diferencia;
+            double diasxbisiesto=0;
+         
+            int i;
 
             Console.Write("Ingrese fecha de nacimiento DD/MM/AAAA: ");
             fechaNacimiento = DateTime.Parse(Console.ReadLine());
-            Console.WriteLine("La fecha de nacimiento es: "+ fechaNacimiento);
-
+            Console.WriteLine("La fecha de nacimiento es: " + fechaNacimiento);
             // Hoy            
             hoy = DateTime.Now;
-            Console.WriteLine("La fecha de hoy es: "+ hoy);
+            Console.WriteLine("La fecha de hoy es: " + hoy);
 
-          
+            for (i = fechaNacimiento.Year; i <= hoy.Year; i++)
+            {
+                if (((i % 4 == 0) && (i % 100 != 0)) || (i % 400 == 0))
+                {
+                    diasxbisiesto++;
+                    //Console.WriteLine(diasxbisiesto);
+                }
+            }
+
             diferencia = hoy - fechaNacimiento;
-            Console.WriteLine("Los dias de vida vividos son: "+ diferencia.Days);
+            diferencia += (TimeSpan.FromDays(diasxbisiesto));
+            
+            Console.WriteLine("Los dias de vida vividos son: " + diferencia.Days);
             Console.ReadLine();
         }
     }
