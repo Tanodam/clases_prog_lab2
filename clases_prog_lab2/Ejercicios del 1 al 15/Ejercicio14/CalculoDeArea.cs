85ec7f6f84c335919778cb2d3a5c86c8c142d1cb
tanodam <desariod95@gmail.com>
2019-03-27 22:03:53 -0300

    Ejercicio 14 finito

---
+++ clases_prog_lab2/Ejercicios del 1 al 15/Ejercicio14/CalculoDeArea.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio14
+{
+    class CalculoDeArea
+    {
+        public static double CalcularCuadrado(double ladoCuadrado)
+        {
+            double resultado = ladoCuadrado * ladoCuadrado;
+            return resultado;
+
+        }
+        public static double CalcularTriangulo(double sabe, double altura)
+        {
+            double resultado = sabe * altura;
+            return resultado;
+        }
+        public static double CalcularCirculo(double radio)
+        {
+            double area;
+            double pi;
+
+            pi = Convert.ToDouble(3.1416);
+            area = pi * radio * radio;
+            return area;
+        }
+    }
+}
