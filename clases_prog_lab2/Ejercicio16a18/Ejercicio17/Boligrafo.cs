62d78be4d324f1c956bc1b74bcf47e25adf03c4b
tanodam <desariod95@gmail.com>
2019-03-30 19:15:55 -0300

    Ejercicio17 terminado

--- clases_prog_lab2/Ejercicio16a18/Ejercicio17/Boligrafo.cs
+++ clases_prog_lab2/Ejercicio16a18/Ejercicio17/Boligrafo.cs
@@ -11,9 +11,11 @@ namespace Ejercicio17
         public short CantidadTintaMaxima = 100;
         private ConsoleColor color;
         private short tinta;
+
         public Boligrafo(short tinta, ConsoleColor color)
         {
-
+            this.tinta = tinta;
+            this.color = color;
         }
         public ConsoleColor GetColor()
         {
@@ -24,34 +26,58 @@ namespace Ejercicio17
             return this.tinta;
 
         }
-        public bool Pintar(int gasto, out string dibujo)
+        public void Pintar(int gasto, out string dibujo)
         {
+            Console.ForegroundColor = this.GetColor();
 
-            if (gasto < this.tinta)
+            dibujo = "";
+            //Console.WriteLine("Nivel de tinta actual {0}", this.tinta);
+            //Console.WriteLine("Usted solicito pintar {0} caracteres", gasto);
+            if (this.tinta > 0)
             {
-                for (int i = 0; i < gasto; i++)
+                for (int i = 0; this.tinta > 0 && i < gasto; i++)
                 {
-                    if (i != 0)
-                    {
-                        str += "**";
-                    }
-                    Console.WriteLine(str);
-                    return true;
+                    SetTinta(Convert.ToInt16(this.tinta - 1));
+                    dibujo += "*";
+                }
+                Console.WriteLine("Usted esta pintando en {0}", this.GetColor());
+                Console.WriteLine("PINTANDO..");
+                Console.WriteLine(dibujo);
+                Console.WriteLine("Pude pintar {0} caracteres", dibujo.Length);
+                Console.WriteLine("Nivel de tinta despues de pintar {0}", this.tinta);
+                Console.WriteLine("-------------");
             }
+            else
+            {
+                Console.WriteLine("No se pudo pintar. Nivel de tinta insuficiente");
+                this.Recargar();
+
+            }
+            Console.ResetColor(); //Restea los colores
+
         }
-        public bool Recargar()
-        {
-            SetTinta(CantidadTintaMaxima);
-            return;
-        }
-        private void SetTinta(short tinta)
+        private bool SetTinta(short tinta)
         {
             if (this.tinta + tinta < CantidadTintaMaxima || this.tinta + tinta > 0)
             {
-
-
+                this.tinta = tinta;
+                return true;
             }
-
+            else
+                return false;
         }
+        public bool Recargar()
+        {
+            Console.WriteLine("\nRecargando tinta de color {0}", this.color);
+            Console.WriteLine("Nivel de tinta actual {0}", this.tinta);
+            if (SetTinta(CantidadTintaMaxima))
+            {
+                Console.WriteLine("Nivel de tinta despues de recargar {0}\n", this.tinta);
+                return true;
+            }
+            else
+                return false;
+        }
+
     }
 }
--- clases_prog_lab2/Ejercicio16a18/Ejercicio17/Program.cs
+++ clases_prog_lab2/Ejercicio16a18/Ejercicio17/Program.cs
@@ -8,10 +8,20 @@ namespace Ejercicio17
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
-            Boligrafo boligrafoAzul = new Boligrafo(100, ConsoleColor.Blue);
+            Boligrafo boligrafoAzul = new Boligrafo(12, ConsoleColor.Blue);
             Boligrafo boligrafoRoja = new Boligrafo(50, ConsoleColor.Red);
+            string dibujo;
+
+            boligrafoAzul.Pintar(10, out dibujo);
+            boligrafoRoja.Pintar(47, out dibujo);
+            boligrafoAzul.Pintar(3, out dibujo);
+            boligrafoAzul.Pintar(3, out dibujo);
+            boligrafoAzul.Pintar(20, out dibujo);
+
+            Console.ReadKey();
         }
     }
 }
