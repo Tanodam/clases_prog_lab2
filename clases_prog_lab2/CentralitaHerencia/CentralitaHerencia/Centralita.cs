0d7e7e716aa5992999603bf142f3269874d09a8d
tanodam <desariod95@gmail.com>
2019-04-24 21:49:32 -0300

    37 finish

---
+++ clases_prog_lab2/CentralitaHerencia/CentralitaHerencia/Centralita.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CentralitaHerencia
+{
+    public class Centralita : Llamada
+    {
+        private List<Llamada> listaDeLlamadas;
+        private string razonSocial;
+
+        #region Propiedades
+
+        public float GananciasPorLocal
+        {
+            get
+            {
+                float ganancia = 0;
+
+                foreach (Llamada llamada in this.listaDeLlamadas)
+                {
+                    if(llamada is Local)
+                    {
+                    Local local = (Local)(llamada);
+                    ganancia += local.CostoLlamada;
+                    }
+                }
+                return ganancia;
+            }
+        }
+
+        public float GananciasPorProvincial
+        {
+            get
+            {
+                float ganancia = 0;
+
+                foreach (Llamada llamada in this.listaDeLlamadas)
+                {
+                    if (llamada is Provincial)
+                    {
+                        Provincial provincial = (Provincial)(llamada);
+                        ganancia += provincial.CostoLlamada;
+                    }
+                }
+                return ganancia;
+            }
+        }
+
+        public float GananciasPorTotal
+        {
+            get
+            {
+                float ganancia = 0;
+
+                foreach (Llamada llamada in this.Llamadas)
+                {
+                    ganancia += this.GananciasPorLocal + this.GananciasPorProvincial;
+                }
+                return ganancia;
+            }
+        }
+
+        public List<Llamada> Llamadas
+        {
+            get
+            {
+                return this.listaDeLlamadas;
+            }
+        }
+
+        #endregion
+
+        #region Métodos
+
+        public Centralita()
+        {
+            this.listaDeLlamadas = new List<Llamada>();
+        }
+
+        public Centralita(string nombreEmpresa) : this()
+        {
+            this.razonSocial = nombreEmpresa;
+        }
+
+        private float CalcularGanancia(TipoLlamada tipo) //ANALIZAR DE DONDE VIENE TIPO DE LLAMADA
+        {
+            switch (tipo)
+            {
+                case Llamada.TipoLlamada.Local:
+                    return this.GananciasPorLocal;
+
+                case Llamada.TipoLlamada.Provincial:
+                    return this.GananciasPorProvincial;
+
+                case Llamada.TipoLlamada.Todas:
+                    return this.GananciasPorTotal;
+                default:
+                    return 0;
+
+            }
+        }
+
+        public new string Mostrar()
+        {
+            StringBuilder datos = new StringBuilder("");
+
+
+            datos.Append("\nRAZÓN SOCIAL: " + this.razonSocial);
+            datos.Append("\nGANANCIAS POR LLAMADAS LOCALES: " + this.CalcularGanancia(TipoLlamada.Local));
+            datos.Append("\nGANANCIAS POR LLAMADAS PROVINCIALES: " + this.CalcularGanancia(TipoLlamada.Provincial));
+            datos.Append("\nGANANCIA TOTAL: " + this.CalcularGanancia(TipoLlamada.Todas));
+
+            foreach (Llamada llamada in this.Llamadas)
+            {
+                datos.Append("\n" + llamada.Mostrar());
+            }
+            return datos.ToString();
+        }
+
+        public void OrdenarLlamadas()
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ clases_prog_lab2/CentralitaHerencia/CentralitaHerencia/Provincial.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+
+    public class Provincial : Llamada
+    {
+        private Franja franjaHoraria;
+
+    public enum Franja
+    {
+        Franja_1,
+        Franja_2,
+        Franja_3,
+    }
+        #region Propiedades
+
+        public float CostoLlamada
+        {
+            get
+            {
+                return this.CalcularCosto();
+            }
+        }
+
+        #endregion
+
+        #region Métodos
+
+        private float CalcularCosto()
+        {
+            float costoTotal = 0;
+
+            switch (this.franjaHoraria)
+            {
+                case Franja.Franja_1:
+                    costoTotal = (float)(this.Duracion * 0.99);
+                    break;
+                case Franja.Franja_2:
+                    costoTotal = (float)(this.Duracion * 1.25);
+                    break;
+                case Franja.Franja_3:
+                    costoTotal = (float)(this.Duracion * 0.66)  ;
+                    break;
+            }
+            return costoTotal;
+        }
+
+        public Provincial(Franja miFranja, Llamada llamada) : this(llamada.NroOrigen, miFranja, llamada.Duracion, llamada.NroDestino)
+        {
+            this.franjaHoraria = miFranja;
+        }
+
+        public Provincial(string origen, Franja miFranja, float duracion, string destino)
+        {
+            this.duracion = duracion;
+            this.nroDestino = destino;
+            this.nroOrigen = origen;
+        }
+
+        public new string Mostrar()
+        {
+            StringBuilder datos = new StringBuilder("");
+
+            datos.Append(base.Mostrar());
+            datos.Append(this.CostoLlamada.ToString());
+            datos.Append(this.franjaHoraria.ToString());
+
+            return datos.ToString();
+        }
+
+        #endregion
+    }
+}
