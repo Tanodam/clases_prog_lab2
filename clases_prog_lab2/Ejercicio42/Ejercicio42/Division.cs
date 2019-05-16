68003b27f80b0055fd11ae0bd8bdb7c112dac044
tanodam <desariod95@gmail.com>
2019-05-15 22:03:21 -0300

    Ejercicio 41

---
+++ clases_prog_lab2/Ejercicio42/Ejercicio42/Division.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio42
+{
+    public class Division
+    {
+        double resultado;
+        public Division(int numeroUno, int numeroDos)
+        {
+            try
+            {
+                if (numeroDos == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+                else
+                {
+                    this.resultado = numeroUno / numeroDos;
+                }
+            }
+            catch (DivideByZeroException e)
+            {
+                throw e;
+            }
+    
+
+        }
+        public Division(int numeroUno, double numeroDos) : this(numeroUno,Convert.ToInt32(numeroDos))
+        {
+            try
+            {
+                Division division = new Division(10, 0);
+
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("ERRRRRRRRRRRRRRROR");
+            }
+        }
+    }
+}
---
+++ clases_prog_lab2/Ejercicio42/Ejercicio42/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 0;
+            Division division = new Division(a, b);
+            //Dividir(a, b);
+            //Console.ReadKey();
+
+            //void Dividir(int numeroUno, int numeroDos)
+            //{
+            //    try
+            //    {
+            //        if (numeroDos == 0)
+            //        {;
+            //            throw new DivideByZeroException();
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine(numeroUno / numeroDos);
+            //        }
+
+            //    }
+            //    catch (DivideByZeroException e)
+            //    {
+            //        Console.WriteLine(e.Message);
+            //    }
+            }
+        }
+    }
+
