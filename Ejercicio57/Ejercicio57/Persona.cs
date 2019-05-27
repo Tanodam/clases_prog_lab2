0a09b5e702bb2d97b459dc6d990cce6fd8b42a22
Soporte TI (int.4733) <soporte3@sullair.com.ar>
2019-05-27 11:47:11 -0300

    Ejercicio 57 falta punto D

---
+++ Ejercicio57/Ejercicio57/Persona.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+namespace Ejercicio57
+{
+    [Serializable]
+    public class Persona
+    {
+        private string nombre;
+        private string apellido;
+
+        public Persona(string nombre, string apellido)
+        {
+            this.apellido = apellido;
+            this.nombre = nombre;
+        }
+        public static void Guardar(Persona persona)
+        {
+            //Creo el objeto serializador
+            BinaryFormatter personaSerializer = new BinaryFormatter();
+            //Indico ubicacion del archuvo y modo de apertura
+            FileStream archivo = new FileStream("Persona.txt",FileMode.Create);
+            //Serializo
+            personaSerializer.Serialize(archivo, persona);
+            //Cierro el archivo
+            archivo.Close();
+        }
+        public static Persona Leer()
+        {
+            Persona persona;
+            //Creo el objeto serializador
+            BinaryFormatter personaDeserializer = new BinaryFormatter();
+            //Indico ubicacion del archuvo y modo de apertura
+            FileStream archivo = new FileStream("Persona.txt", FileMode.Open);
+            //Deserializo
+            persona = (Persona)personaDeserializer.Deserialize(archivo);
+            //Cierro el archivo
+            archivo.Close();
+            return persona;
+        }
+        public override string ToString()
+        {
+            return String.Format("Nombre Apellido: {0} {1}", this.nombre, this.apellido);
+        }
+
+
+    }
+}
---
+++ Ejercicio57/Ejercicio57/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*En el Main instanciar un objeto del tipo Persona e intentar serializarlo.*/
+            Persona personaUno = new Persona("Damian", "Desario");
+            Persona.Guardar(personaUno);
+            /*Luego intentar leer ese objeto serializado en una nueva instancia de persona y lo muestre por pantalla.*/
+            Persona personaDos = Persona.Leer();
+            Console.Write(personaDos.ToString());
+            Console.ReadKey();
+        }
+    }
+}
