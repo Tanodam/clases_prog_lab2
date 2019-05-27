3b19b7334a50c688170574f086286aff2099408c
Soporte TI (int.4733) <soporte3@sullair.com.ar>
2019-05-27 17:12:44 -0300

    Ejercicio 58 no miren esto, da pena

---
+++ Ejercicio58/IO/PuntoTxt.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IO
+{
+    [Serializable]
+    public class PuntoTxt : Archivo, IArchivos<PuntoTxt>
+    {
+        public bool Guardar(string ruta, PuntoTxt objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GuardarComo(string ruta, PuntoTxt objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PuntoTxt Leer(string ruta)
+        {
+            throw new NotImplementedException();
+        }
+        #region Metodos
+
+        protected override bool validarArchivo(string ruta, bool validarExistencia)
+        {
+            if (Path.GetExtension(ruta) != ".txt")
+            {
+                throw new ArchivoIncorrectoException("El archivo no es un txt");
+            }
+            try
+            {
+                File.Exists(ruta);
+                return true;
+            }
+            catch (FileNotFoundException exception)
+            {
+                throw new ArchivoIncorrectoException("El archivo no es correcto", exception);
+            }
+            
+        }
+        #endregion
+    }
+}
